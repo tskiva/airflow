8bf213127d9dbbe8b0f72799c867ee44764a3535
Tiago Schievenin <tschievenin@tiago-go2c-mac.local>
2024-09-21 07:41:47 -0300

    teste pismo

---
+++ config/init.sql
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER ariflow;
+    CREATE DATABASE airflow;
+    GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
+EOSQL
+
